2fc9fc19544e654f664809bd0b1572e62a22eafb
Jakub Syrek <jaqb.syrek@gmail.com>
2019-07-14 00:00:04 +0200

    Powershell script added in control Flow

--- Integration Services ETL/DATA_SETS/University_db/Convert-Data-To-Csv.ps1
+++ Integration Services ETL/DATA_SETS/University_db/Convert-Data-To-Csv.ps1
@@ -1,12 +1,4 @@
 ﻿
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{ 
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-Break
-}
-
-Set-ExecutionPolicy -ExecutionPolicy UnRestricted
 
 $scriptpath = $MyInvocation.MyCommand.Path
 
@@ -82,4 +74,4 @@ foreach($file in (Get-ChildItem $dir ))
     }
   }
 }
- pause
+ 
