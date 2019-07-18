2a07524f31f56878134fe43f8219e0c40322e5c0
Jakub Syrek <jaqb.syrek@gmail.com>
2019-07-18 22:45:13 +0200

    5

--- Integration Services WizyWydane/Convert-Data-To-Csv.ps1
+++ Integration Services WizyWydane/Convert-Data-To-Csv.ps1
@@ -4,22 +4,20 @@ $scriptpath = $MyInvocation.MyCommand.Path
 
 $dir = Split-Path $scriptpath
 
+$originalCsv = $dir + "\_origCsv\"
 
 
-
-foreach($file in (Get-ChildItem $dir )) {
+foreach($file in (Get-ChildItem $originalCsv )) {
   if ($file.FullName.Contains("csv"))
   {
   
-      $output = $dir + "\1" + $file 
+      $output = $dir + "\" + $file 
+      
       (Get-Content $file.FullName) | Foreach-Object {$_ -replace ","," "} | Foreach-Object {$_ -replace ';', ','} |  Foreach-Object {$_ -replace ' ', ''} |Out-File $output -Encoding ascii
-      #$fil = Get-Content $output  #$file.FullName 
-      #$fil = $fil.Replace(",Zast?pienie naklejki wizowej, Rodzaj wizy - VD,Zast?pienie naklejki wizowej, Rodzaj wizy - VC BiOSG","`r`n")
-      #$fil | Out-File $output -Encoding ascii
-      Write-Host $fil     
-      Copy $output $file.FullName
-      Remove-Item $output -Force 
-      Write-Host $file " cleansed from quotations "    
+           
+      #Copy $output $file.FullName
+      #Remove-Item $output -Force 
+      Write-Host $output " cleansed from quotations "    
   }
 }
 
