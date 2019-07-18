476ebaf1b0a495946a20b0d78db13443cdc6c9ed
Jakub Syrek <jaqb.syrek@gmail.com>
2019-07-18 14:56:26 +0200

    Poprawki do krypy czyszczacego i do procesu

---
+++ Integration Services WizyWydane/Convert-Data-To-Csv.ps1
@@ -0,0 +1,27 @@
+﻿
+
+$scriptpath = $MyInvocation.MyCommand.Path
+
+$dir = Split-Path $scriptpath
+
+
+
+
+foreach($file in (Get-ChildItem $dir )) {
+  if ($file.FullName.Contains("csv"))
+  {
+  
+      $output = $dir + "\1" + $file 
+      (Get-Content $file.FullName) | Foreach-Object {$_ -replace ","," "} | Foreach-Object {$_ -replace ';', ','} |  Foreach-Object {$_ -replace ' ', ''} |Out-File $output -Encoding ascii
+      #$fil = Get-Content $output  #$file.FullName 
+      #$fil = $fil.Replace(",Zast?pienie naklejki wizowej, Rodzaj wizy - VD,Zast?pienie naklejki wizowej, Rodzaj wizy - VC BiOSG","`r`n")
+      #$fil | Out-File $output -Encoding ascii
+      Write-Host $fil     
+      Copy $output $file.FullName
+      Remove-Item $output -Force 
+      Write-Host $file " cleansed from quotations "    
+  }
+}
+
+
+
